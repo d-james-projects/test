3da055956adf66c7a3f05fe4e5367b76f933faaa
David J <dave@no.com>
2021-03-25 22:22:51 +0000

    jenkins

--- library-folder/vars/aUsefulFunc.groovy
+++ library-folder/vars/aUsefulFunc.groovy
@@ -1,11 +1,15 @@
 #!groovy
 
+def funcInLibrary() {
+	return 42
+}
+
 def call(def theThingPassed = "") {
 	echo "This function is called aUsefulFunc"
         echo theThingPassed
 	echo funcInLibrary
 }
 
-def funcInLibrary() {
+/*def funcInLibrary() {
 	return 42
-}
+}*/
